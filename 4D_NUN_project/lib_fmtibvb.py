01827cf6950a418b88c0566632c3008480487fdd
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2022-10-25 16:37:17 -0700

    Add files via upload

---
+++ 4D_NUN_project/lib_fmtibvb.py
@@ -0,0 +1,203 @@
+# A small library of functions to read and write .fmt / .ib / .vb files into and out of
+# python structures that are JSON serializable.
+#
+# GitHub eArmada8/gust_stuff
+
+import re, struct, json
+
+# Currently only simple formats (8-, 16-, and 32-bit) are supported.  Floats must be 32-bit.
+# Attempting to read an unsupported format will return a raw bytes object.
+def unpack_dxgi_vector(f, stride, dxgi_format):
+    dxgi_format = dxgi_format.split('DXGI_FORMAT_')[-1]
+    dxgi_format_split = dxgi_format.split('_')
+    if len(dxgi_format_split) == 2:
+        numtype = dxgi_format_split[1]
+        vec_format = re.findall("[0-9]+",dxgi_format_split[0])
+        if len(vec_format) > 0:
+            vec_bits = int(vec_format[0])
+            vec_elements = len(vec_format)
+        else:
+            vec_bits = 0
+            vec_elements = 0
+    else:
+        numtype = 'UNSUPPORTED'
+
+    if numtype == 'FLOAT' and vec_bits == 32 and (vec_elements * vec_bits / 8 == stride):
+        read = list(struct.unpack("<"+str(vec_elements)+"f", f.read(stride)))
+    elif numtype == 'UINT' and (vec_elements * vec_bits / 8 == stride):
+        if vec_bits == 32:
+            read = list(struct.unpack("<"+str(vec_elements)+"I", f.read(stride)))
+        elif vec_bits == 16:
+            read = list(struct.unpack("<"+str(vec_elements)+"H", f.read(stride)))
+        elif vec_bits == 8:
+            read = list(struct.unpack("<"+str(vec_elements)+"B", f.read(stride)))
+    elif numtype == "SINT" and (vec_elements * vec_bits / 8 == stride):
+        if vec_bits == 32:
+            read = list(struct.unpack("<"+str(vec_elements)+"i", f.read(stride)))
+        elif vec_bits == 16:
+            read = list(struct.unpack("<"+str(vec_elements)+"h", f.read(stride)))
+        elif vec_bits == 8:
+            read = list(struct.unpack("<"+str(vec_elements)+"b", f.read(stride)))
+    else:
+        read = f.read(stride)
+    return (read)
+
+def pack_dxgi_vector(f, data, stride, dxgi_format):
+    dxgi_format = dxgi_format.split('DXGI_FORMAT_')[-1]
+    dxgi_format_split = dxgi_format.split('_')
+    if len(dxgi_format_split) == 2:
+        numtype = dxgi_format_split[1]
+        vec_format = re.findall("[0-9]+",dxgi_format_split[0])
+        if len(vec_format) > 0:
+            vec_bits = int(vec_format[0])
+            vec_elements = len(vec_format)
+        else:
+            vec_bits = 0
+            vec_elements = 0
+    else:
+        numtype = 'UNSUPPORTED'
+
+    if numtype == 'FLOAT' and vec_bits == 32 and (vec_elements * vec_bits / 8 == stride):
+        for i in range(vec_elements):
+            f.write(struct.pack("<f", data[i]))
+    elif numtype == 'UINT' and (vec_elements * vec_bits / 8 == stride):
+        for i in range(vec_elements):
+            if vec_bits == 32:
+                f.write(struct.pack("<I", data[i]))
+            elif vec_bits == 16:
+                f.write(struct.pack("<H", data[i]))
+            elif vec_bits == 8:
+                f.write(struct.pack("<B", data[i]))
+    elif numtype == "SINT" and (vec_elements * vec_bits / 8 == stride):
+        for i in range(vec_elements):
+            if vec_bits == 32:
+                f.write(struct.pack("<i", data[i]))
+            elif vec_bits == 16:
+                f.write(struct.pack("<h", data[i]))
+            elif vec_bits == 8:
+                f.write(struct.pack("<b", data[i]))
+    else:
+        write = f.write(data)
+    return
+
+def read_fmt(fmt_filename):
+    fmt_struct = {}
+    with open(fmt_filename, 'r') as f:
+        elements = []
+        while True:
+            line = f.readline().strip()
+            if line[0:7] == 'element':
+                element = {}
+                element['id'] = line.split('[')[-1][:-2]
+                while True:
+                    line_offset = f.tell()
+                    line = f.readline().strip()
+                    if line[0:7] == 'element' or line == "":
+                        f.seek(line_offset)
+                        elements.append(element)
+                        break
+                    else:
+                        element[line.split(': ')[0]] = line.split(': ')[1]
+            else:
+                if line == "":
+                    break
+                fmt_struct[line.split(': ')[0]] = line.split(': ')[1]
+        fmt_struct['elements'] = elements
+    return(fmt_struct)
+
+def write_fmt(fmt_struct, fmt_filename):
+    output = bytearray()
+    for key in fmt_struct:
+        if key == "elements":
+            for i in range(len(fmt_struct["elements"])):
+                for key in fmt_struct["elements"][i]:
+                    if key == "id":
+                        output.extend(("element[" + fmt_struct["elements"][i][key] + "]:\r\n").encode())
+                    else:
+                        output.extend(("  " + key + ": " + fmt_struct["elements"][i][key] + "\r\n").encode())
+        else:
+            output.extend((key + ": " + fmt_struct[key] + "\r\n").encode())
+    with open(fmt_filename, "wb") as f:
+        f.write(output)
+    return
+
+def read_ib(ib_filename, fmt_struct):
+    ib_data = []
+    # Cheating a bit here, since all index buffers I've seen are single numbers, but fmt doesn't have a stride for IB
+    ib_stride = int(int(re.findall("[0-9]+", fmt_struct["format"])[0])/8)
+    with open(ib_filename, 'rb') as f:
+        length = f.seek(0,2)
+        f.seek(0)
+        vertex_num = 0
+        triangle = []
+        while f.tell() < length:
+            triangle.extend(unpack_dxgi_vector(f, ib_stride, fmt_struct["format"]))
+            vertex_num += 1
+            if vertex_num % 3 == 0:
+                ib_data.append(triangle)
+                triangle = []
+    return(ib_data)
+
+def write_ib(ib_data, ib_filename, fmt_struct):
+    # See above about cheating
+    ib_stride = int(int(re.findall("[0-9]+", fmt_struct["format"])[0])/8)
+    with open(ib_filename, 'wb') as f:
+        for i in range(len(ib_data)):
+            for j in range(len(ib_data[i])):
+                pack_dxgi_vector(f, [ib_data[i][j]], ib_stride, fmt_struct["format"])
+    return
+
+def read_vb(vb_filename, fmt_struct):
+    vb_data = []
+    with open(vb_filename, 'rb') as f:
+        length = f.seek(0,2)
+        f.seek(0)
+        num_vertex = int(length / int(fmt_struct["stride"]))
+        buffer_strides = []
+        # Calculate individual buffer strides
+        for i in range(len(fmt_struct["elements"])):
+            if i == len(fmt_struct["elements"]) - 1:
+                buffer_strides.append(int(fmt_struct["stride"]) - int(fmt_struct["elements"][i]["AlignedByteOffset"]))
+            else:
+                buffer_strides.append(int(fmt_struct["elements"][i+1]["AlignedByteOffset"]) \
+                    - int(fmt_struct["elements"][i]["AlignedByteOffset"]))
+        # Read in the buffers
+        for i in range(len(fmt_struct["elements"])):
+            element = {}
+            element["SemanticName"] = fmt_struct["elements"][i]["SemanticName"]
+            element_buffer = []
+            for j in range(num_vertex):
+                f.seek(j * int(fmt_struct["stride"]) + int(fmt_struct["elements"][i]["AlignedByteOffset"]),0)
+                element_buffer.append(unpack_dxgi_vector(f, buffer_strides[i], fmt_struct["elements"][i]["Format"]))
+            element["Buffer"] = element_buffer
+            vb_data.append(element)
+    return(vb_data)
+
+def write_vb(vb_data, vb_filename, fmt_struct):
+    with open(vb_filename, 'wb') as f:
+        buffer_strides = []
+        # Calculate individual buffer strides
+        for i in range(len(fmt_struct["elements"])):
+            if i == len(fmt_struct["elements"]) - 1:
+                buffer_strides.append(int(fmt_struct["stride"]) - int(fmt_struct["elements"][i]["AlignedByteOffset"]))
+            else:
+                buffer_strides.append(int(fmt_struct["elements"][i+1]["AlignedByteOffset"]) \
+                    - int(fmt_struct["elements"][i]["AlignedByteOffset"]))
+        # Write out the buffers, vertex by vertex.
+        for j in range(len(vb_data[0]["Buffer"])):
+            for i in range(len(fmt_struct["elements"])):
+                pack_dxgi_vector(f, vb_data[i]["Buffer"][j], buffer_strides[i], fmt_struct["elements"][i]["Format"])
+    return
+
+# The following two functions are purely for convenience
+def read_struct_from_json(filename):
+    with open(filename, 'r') as f:
+        return(json.loads(f.read()))
+
+def write_struct_to_json(struct, filename):
+    if not filename[:-5] == '.json':
+        filename += '.json'
+    with open(filename, "wb") as f:
+        f.write(json.dumps(struct, indent=4).encode("utf-8"))
+    return
+
