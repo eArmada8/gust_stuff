03a17257d297bbf32a432ef44f964d6cdafd9768
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2022-10-27 21:31:34 -0700

    Added absolute quaternion/position calculations
    Removed all matrix math code, rotations and transformations are now performed exclusively with the pyquaternion library, as G1M stores all bone orientation as quaternion anyway.  Hopefully this will increase accuracy.

--- 4D_NUN_project/g1m_skel_extract.py
+++ 4D_NUN_project/g1m_skel_extract.py
@@ -1,6 +1,16 @@
 # Skeleton data extractor for G1M files.
 # Based entirely off the work of Joschuka (fmt_g1m / Project G1M), huge thank you to Joschuka!
 #
+# Transforming rotation and position of bones from reltive positions to absolute posistions seems to get
+# slightly different results than Noesis libraries; I don't have access to original source code but it
+# makes sense to me to use quaternions exclusively rather than converting everything to matrices since
+# G1M stores the skeletons using quaternions.  Hopefully this is more accurate, rather than less.  Final 
+# transformation matrices are included in 4x3 format for convenience.
+#
+# Requires both numpy and pyquaternion
+# These can be installed by:
+# /path/to/python3 -m pip install numpy quaternion
+#
 # Steps:
 # 1. Extract fmt/ib/vb from desired g1m directories with g1m_export.exe 
 # 2. Run this script (in the folder with the g1m file).
@@ -8,13 +18,7 @@
 # GitHub eArmada8/gust_stuff
 
 import glob, os, io, sys, struct, json, numpy
-
-def quaternionto3x3matrix(q): #[x,y,z,w]
-    return(numpy.array([\
-        [2*(q[3]*q[3]+q[0]*q[0])-1, 2*(q[0]*q[1]-q[3]*q[2]), 2*(q[0]*q[2]+q[3]*q[1])], \
-        [2*(q[0]*q[1]+q[3]*q[2]), 2*(q[3]*q[3]+q[1]*q[1])-1, 2*(q[1]*q[2]-q[3]*q[0])], \
-        [2*(q[0]*q[2]-q[3]*q[1]), 2*(q[1]*q[2]+q[3]*q[0]), 2*(q[3]*q[3]+q[2]*q[2])-1] \
-        ]))
+from pyquaternion import Quaternion
 
 def parseG1MS(g1ms_chunk,e):
     g1ms_section = {}
@@ -43,14 +47,47 @@ def parseG1MS(g1ms_chunk,e):
             bone['scale'] = struct.unpack(e+"3f",f.read(12))
             bone['parentID'], = struct.unpack(e+"i", f.read(4))
             bone['rotation_q'] = list(struct.unpack(e+"4f",f.read(16))) # x,y,z,w
+            bone['q_wxyz'] = bone['rotation_q'][-1:]+bone['rotation_q'][:-1] # x,y,z,w
+            quat = Quaternion(bone['q_wxyz']) # w,x,y,z
             bone['position'] = list(struct.unpack(e+"4f",f.read(16))) # x,y,z,w
-            bone['boneMatrixTransform'] = numpy.r_[numpy.linalg.inv(quaternionto3x3matrix(bone['rotation_q'])), \
-            [bone['position'][0:3]]].tolist()
+            bone['pos_xyz'] = bone['position'][0:3]
+            bone['boneMatrixTransform'] = quat.transformation_matrix
+            bone['boneMatrixTransform'][3] = bone['position']
+            bone['boneMatrixTransform'] = numpy.delete(bone['boneMatrixTransform'], -1, 1)
+            bone['boneMatrixTransform'] = bone['boneMatrixTransform'].tolist()
             localBoneMatrices.append(bone['boneMatrixTransform'])
             boneList.append(bone)
         g1ms_section["localBoneMatrices"] = localBoneMatrices
         g1ms_section["boneList"] = boneList
     return(g1ms_section)
+
+# Takes quat/pos relative to parent, and reorients / moves to be relative to the origin.
+# Parent bone must already be transformed.
+def calc_abs_rotation_position(bone, parent_bone):
+    q1 = Quaternion(bone['q_wxyz'])
+    qp = Quaternion(parent_bone['abs_q'])
+    bone["abs_q"] = list(q1 * qp)
+    bone["abs_p"] = rotated_p = (numpy.array(qp.rotate(bone['pos_xyz'])) + parent_bone['abs_p']).tolist()
+    bone["abs_tm"] = Quaternion(bone["abs_q"]).transformation_matrix.tolist()
+    bone["abs_tm"] = numpy.delete(bone["abs_tm"], -1, 1)
+    bone["abs_tm"][3] = bone["abs_p"]
+    bone["abs_tm"] = bone["abs_tm"].tolist()
+    return(bone)
+
+# In fmt_g1m, used only on primary skeleton; this function is performed in combine_skeleton() for 2nd layer
+def calc_abs_skeleton(base_skel_data):
+    for bone in range(len(base_skel_data['boneList'])):
+        parentId = base_skel_data['boneList'][bone]['parentID']
+        if parentId > -1:
+            current_bone = base_skel_data['boneList'][bone]
+            parent_bone = [x for x in base_skel_data['boneList'] if x['i'] == parentId][0]
+            # Transform from relative rotation / position to absolute
+            base_skel_data['boneList'][bone] = calc_abs_rotation_position(current_bone, parent_bone)
+        else: #First bone (bone_0) is already absolute, no need to transform
+            base_skel_data['boneList'][bone]['abs_q'] = base_skel_data['boneList'][bone]['q_wxyz']
+            base_skel_data['boneList'][bone]['abs_p'] = base_skel_data['boneList'][bone]['pos_xyz']
+            base_skel_data['boneList'][bone]['abs_tm'] = base_skel_data['boneList'][bone]['boneMatrixTransform']
+    return(base_skel_data)
 
 def combine_skeleton(base_skel_data, model_skel_data):
     if model_skel_data['jointIndicesCount'] == len(base_skel_data['boneIDList']):
@@ -67,20 +104,14 @@ def combine_skeleton(base_skel_data, model_skel_data):
                 combined_data['boneToBoneID'][id + externalOffsetList] = i
         for i in range(model_skel_data["jointCount"]):
             bone = model_skel_data['boneList'][i]
+            bone['i'] = i + externalOffsetList
+            bone['bone_id'] = 'Clothbone_' + str(i)
             if bone['parentID'] < 0:
-                bone['i'] = i + externalOffsetList
-                bone['bone_id'] = 'Clothbone_' + str(i)
                 bone['parentID'] = bone['parentID'] & 0xFFFF
-                parent_bone = [x for x in combined_data['boneList'] if x['i'] == bone['parentID']][0]
-                bone['boneMatrixTransform'] = (numpy.array(bone['boneMatrixTransform']) * numpy.array(parent_bone['boneMatrixTransform'])).tolist()
-                combined_data['boneList'].append(bone)
             else:
-                bone['i'] = i + externalOffsetList
-                bone['bone_id'] = 'Clothbone_' + str(i)
                 bone['parentID'] = bone['parentID'] + externalOffsetList
-                parent_bone = [x for x in combined_data['boneList'] if x['i'] == bone['parentID']][0]
-                bone['boneMatrixTransform'] = (numpy.array(bone['boneMatrixTransform']) * numpy.array(parent_bone['boneMatrixTransform'])).tolist()
-                combined_data['boneList'].append(bone)
+            parent_bone = [x for x in combined_data['boneList'] if x['i'] == bone['parentID']][0]
+            combined_data['boneList'].append(calc_abs_rotation_position(bone, parent_bone))
         return(combined_data)
 
 # The argument passed (g1m_name) is actually the folder name
@@ -129,7 +160,7 @@ if __name__ == "__main__":
         parser.add_argument('g1m_filename', help="Name of g1m file to extract G1MS data (required).")
         args = parser.parse_args()
         if os.path.exists(args.g1m_filename) and args.g1m_filename[-4:].lower() == '.g1m':
-            model_skel_data = parseG1M(args.g1m_filename[:-4])
+            model_skel_data = calc_abs_skeleton(parseG1M(args.g1m_filename[:-4]))
             with open(args.g1m_filename+"_skel.json", "wb") as f:
                 f.write(json.dumps(model_skel_data, indent=4).encode("utf-8"))
     else:
@@ -139,7 +170,7 @@ if __name__ == "__main__":
         if len(models) > 0:
             for i in range(len(models)):
                 # Need to add logic here to detect if skeleton is external
-                base_skel_data = parseG1M(models[i].split("_MODEL_")[0]+'_MODEL')
+                base_skel_data = calc_abs_skeleton(parseG1M(models[i].split("_MODEL_")[0]+'_MODEL'))
                 model_skel_data = parseG1M(models[i])
                 skel_data = combine_skeleton(base_skel_data, model_skel_data)
                 with open(models[i]+"/skel_data.json", "wb") as f:
