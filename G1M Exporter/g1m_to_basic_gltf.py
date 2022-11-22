ccdbc356d6f759be72bd4a558bb3892428271e94
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2022-11-21 21:41:50 -0800

    Added vertex skinning support (skeleton rigging)

--- G1M Exporter/g1m_to_basic_gltf.py
+++ G1M Exporter/g1m_to_basic_gltf.py
@@ -1,4 +1,5 @@
-# Very basic glTF builder for G1M files, still kind of broken and no skeleton code.
+# Very basic glTF builder for G1M files, mostly working now for its intended purpose (skeletal rigging
+# of extracted meshes from g1m_export_meshes.py) although it still has bugs.
 #
 # Based primarily off the work of GitHub/Joschuka and GitHub/three-houses-research-team,
 # huge thank you!  Also many thanks to eterniti for sharing code with me to reference.
@@ -20,7 +21,8 @@
 #
 # GitHub eArmada8/gust_stuff
 
-import glob, os, io, sys, copy, json
+import glob, os, io, sys, copy, json, numpy
+from pyquaternion import Quaternion
 from g1m_export_meshes import *
 
 # This only handles formats compatible with G1M
@@ -57,9 +59,16 @@ def convert_format_for_gltf(dxgi_format):
 def convert_fmt_for_gltf(fmt):
     new_fmt = copy.deepcopy(fmt)
     stride = 0
+    new_semantics = {'BLENDWEIGHT': 'WEIGHTS', 'BLENDINDICES': 'JOINTS'}
+    need_index = ['WEIGHTS', 'JOINTS', 'COLOR', 'TEXCOORD']
     for i in range(len(fmt['elements'])):
+        if new_fmt['elements'][i]['SemanticName'] in new_semantics.keys():
+            new_fmt['elements'][i]['SemanticName'] = new_semantics[new_fmt['elements'][i]['SemanticName']]
         new_info = convert_format_for_gltf(fmt['elements'][i]['Format'])
         new_fmt['elements'][i]['Format'] = new_info['format']
+        if new_fmt['elements'][i]['SemanticName'] in need_index:
+            new_fmt['elements'][i]['SemanticName'] = new_fmt['elements'][i]['SemanticName'] + '_' +\
+                new_fmt['elements'][i]['SemanticIndex']
         new_fmt['elements'][i]['AlignedByteOffset'] = stride
         new_fmt['elements'][i]['componentType'] = new_info['componentType']
         new_fmt['elements'][i]['componentStride'] = new_info['componentStride']
@@ -73,13 +82,52 @@ def convert_fmt_for_gltf(fmt):
     new_fmt['stride'] = stride
     return(new_fmt)
 
+def convert_bones_to_single_file(submesh):
+    bone_element_index = int([x for x in submesh['fmt']['elements'] if x['SemanticName'] == 'BLENDINDICES'][0]['id'])
+    for i in range(len(submesh['vb'][bone_element_index]['Buffer'])):
+        for j in range(len(submesh['vb'][bone_element_index]['Buffer'][i])):
+            submesh['vb'][bone_element_index]['Buffer'][i][j] = \
+                int(submesh['vb'][bone_element_index]['Buffer'][i][j] / 3) # Dunno why G1M indices count by 3
+    return(submesh)
+
+def list_of_utilized_bones(submesh, model_skel_data):
+    true_bone_map = {}
+    if model_skel_data['jointCount'] > 1:
+        for i in range(len(model_skel_data['boneList'])):
+            true_bone_map[model_skel_data['boneList'][i]['bone_id']] = model_skel_data['boneList'][i]['i']
+    return([true_bone_map[x] for x in submesh['vgmap'].keys()])
+
+def expand_weight_groups_as_needed(submesh):
+    new_submesh = copy.deepcopy(submesh)
+    bone_element_index = int([x for x in new_submesh['fmt']['elements'] if x['SemanticName'] == 'BLENDINDICES'][0]['id'])
+    weight_element_index = int([x for x in new_submesh['fmt']['elements'] if x['SemanticName'] == 'BLENDWEIGHT'][0]['id'])
+    if len(new_submesh['vb'][bone_element_index]['Buffer'][0]) - len(new_submesh['vb'][weight_element_index]['Buffer'][0]) > 0:
+        for i in range(len(new_submesh['vb'][bone_element_index]['Buffer'][0]) - len(new_submesh['vb'][weight_element_index]['Buffer'][0])):
+            for j in range(len(new_submesh['vb'][weight_element_index]['Buffer'])):
+                new_submesh['vb'][weight_element_index]['Buffer'][j].append(1-sum(new_submesh['vb'][weight_element_index]['Buffer'][j]))
+                if (new_submesh['vb'][weight_element_index]['Buffer'][j][-1] < 0.00001):
+                    new_submesh['vb'][weight_element_index]['Buffer'][j][-1] = 0
+            prefices = ['R','G','B','A','D']
+            weightformat = new_submesh['fmt']['elements'][weight_element_index]['Format']
+            dxgi_format_split = weightformat.split('_')
+            if len(dxgi_format_split) == 2:
+                numtype = dxgi_format_split[1]
+                vec_format = re.findall("[0-9]+",dxgi_format_split[0])
+                vec_bits = int(vec_format[0])
+                vec_elements = len(vec_format)
+                vec_elements += 1 #Expand by one
+            new_submesh['fmt']['elements'][weight_element_index]['Format'] =\
+                "".join(["{0}{1}".format(prefices[j], vec_bits) for j in range(vec_elements)]) + '_' + numtype
+            #Expand stride and adjust offsets
+            new_submesh['fmt']['stride'] = str(int(int(new_submesh['fmt']['stride']) + vec_bits / 8))
+            for j in range(weight_element_index+1, len(new_submesh['fmt']['elements'])):
+                new_submesh['fmt']['elements'][j]['AlignedByteOffset'] =\
+                    str(int(int(new_submesh['fmt']['elements'][j]['AlignedByteOffset']) + vec_bits / 8))
+    return(new_submesh)
+
 def write_glTF(g1m_name, g1mg_stream, model_mesh_metadata, model_skel_data, e = '<'):
     subvbs = [x for x in model_mesh_metadata['sections'] if x['type'] == "SUBMESH"][0]
     fmts = generate_fmts(model_mesh_metadata)
-    # Reformat variables for glTF (change half-floats, unorms, etc)
-    gltf_fmts = []
-    for i in range(len(fmts)):
-        gltf_fmts.append(convert_fmt_for_gltf(fmts[i]))
     gltf_data = {}
     gltf_data['asset'] = { 'version': '2.0' }
     gltf_data['accessors'] = []
@@ -90,69 +138,113 @@ def write_glTF(g1m_name, g1mg_stream, model_mesh_metadata, model_skel_data, e = 
     gltf_data['scenes'] = [{}]
     gltf_data['scenes'][0]['nodes'] = [0]
     gltf_data['scene'] = 0
+    gltf_data['skins'] = []
     giant_buffer = bytes()
     mesh_nodes = []
     buffer_view = 0
     for i in range(len(model_skel_data['boneList'])):
         node = {'children': [], 'name': model_skel_data['boneList'][i]['bone_id']}
-        if i > 0:
+        if not list(model_skel_data['boneList'][i]['rotation_q']) == [0,0,0,1]:
             node['rotation'] = model_skel_data['boneList'][i]['rotation_q']
+        if not list(model_skel_data['boneList'][i]['scale']) == [1,1,1]:
             node['scale'] = model_skel_data['boneList'][i]['scale']
+        if not list(model_skel_data['boneList'][i]['pos_xyz']) == [0,0,0]:
             node['translation'] = model_skel_data['boneList'][i]['pos_xyz']
+        if i > 0:
             gltf_data['nodes'][model_skel_data['boneList'][i]['parentID']]['children'].append(len(gltf_data['nodes']))
         gltf_data['nodes'].append(node)
+    for i in range(len(gltf_data['nodes'])):
+        if len(gltf_data['nodes'][i]['children']) == 0:
+            del(gltf_data['nodes'][i]['children'])
     for subindex in range(len(subvbs['data'])):
-        # Skip mesh if 4D
-        if [x for x in gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'] if x['SemanticName'] == 'POSITION'][0]['accessor_type'] == 'VEC3':
+        # Skip submesh if 4D
+        if len(re.findall('[0-9]+', [x for x in fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'] if x['SemanticName'] == 'POSITION'][0]['Format'])) == 3:
             primitive = {"attributes":{}}
+            fmts = generate_fmts(model_mesh_metadata) # Refresh FMT every time, to dereference
             submesh = generate_submesh(subindex, g1mg_stream, model_mesh_metadata, model_skel_data, fmts, e=e, cull_vertices = True)
-            mesh_stream = io.BytesIO()
-            write_vb_stream(submesh['vb'], mesh_stream, gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']], e=e, stripe = False)
-            block_offset = 0
-            for element in range(len(gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'])):
-                if len([x for x in gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'] if x['SemanticName'] ==\
-                    gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'][element]['SemanticName']]) > 1:
-                    primitive["attributes"]["{0}_{1}".format(\
-                        gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'][element]['SemanticName'],\
-                        gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'][element]['SemanticIndex'])] =\
-                        len(gltf_data['accessors'])
-                else:
-                    primitive["attributes"][gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'][element]['SemanticName']]\
+            # Skip empty submeshes
+            if len(submesh['ib']) > 0:
+                submesh = convert_bones_to_single_file(submesh)
+                submesh = expand_weight_groups_as_needed(submesh)
+                gltf_fmt = convert_fmt_for_gltf(submesh['fmt'])
+                vb_stream = io.BytesIO()
+                write_vb_stream(submesh['vb'], vb_stream, gltf_fmt, e=e, stripe = False)
+                block_offset = len(giant_buffer)
+                for element in range(len(gltf_fmt['elements'])):
+                    primitive["attributes"][gltf_fmt['elements'][element]['SemanticName']]\
                         = len(gltf_data['accessors'])
+                    gltf_data['accessors'].append({"bufferView" : buffer_view,\
+                        "componentType": gltf_fmt['elements'][element]['componentType'],\
+                        "count": len(submesh['vb'][element]['Buffer']),\
+                        "type": gltf_fmt['elements'][element]['accessor_type']})
+                    if gltf_fmt['elements'][element]['SemanticName'] == 'POSITION':
+                        gltf_data['accessors'][-1]['max'] =\
+                            [max([x[0] for x in submesh['vb'][element]['Buffer']]),\
+                             max([x[1] for x in submesh['vb'][element]['Buffer']]),\
+                             max([x[2] for x in submesh['vb'][element]['Buffer']])]
+                        gltf_data['accessors'][-1]['min'] =\
+                            [min([x[0] for x in submesh['vb'][element]['Buffer']]),\
+                             min([x[1] for x in submesh['vb'][element]['Buffer']]),\
+                             min([x[2] for x in submesh['vb'][element]['Buffer']])]
+                    gltf_data['bufferViews'].append({"buffer": 0,\
+                        "byteOffset": block_offset,\
+                        "byteLength": len(submesh['vb'][element]['Buffer']) *\
+                        gltf_fmt['elements'][element]['componentStride'],\
+                        "target" : 34962})
+                    block_offset += len(submesh['vb'][element]['Buffer']) *\
+                        gltf_fmt['elements'][element]['componentStride']
+                    buffer_view += 1
+                vb_stream.seek(0)
+                giant_buffer += vb_stream.read()
+                vb_stream.close()
+                del(vb_stream)
+                ib_stream = io.BytesIO()
+                write_ib_stream(submesh['ib'], ib_stream, gltf_fmt, e=e)
+                # IB is 16-bit so can be misaligned, unlike VB (which only has 32-, 64- and 128-bit types in G1M)
+                while (ib_stream.tell() % 4) > 0:
+                    ib_stream.write(b'\x00')
+                primitive["indices"] = len(gltf_data['accessors'])
                 gltf_data['accessors'].append({"bufferView" : buffer_view,\
-                    "byteOffset": block_offset,\
-                    "componentType": gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'][element]['componentType'],\
-                    "count": len(submesh['vb'][element]['Buffer']),\
-                    "type": gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'][element]['accessor_type']})
-                block_offset += len(submesh['vb'][element]['Buffer']) *\
-                    gltf_fmts[subvbs['data'][subindex]['vertexBufferIndex']]['elements'][element]['componentStride']
-            write_ib_stream(submesh['ib'], mesh_stream, gltf_fmts[subvbs['data'][subindex]['indexBufferIndex']], e=e)
-            # IB is 16-bit so can be misaligned, unlike VB (which only has 32-, 64- and 128-bit types in G1M)
-            while (mesh_stream.tell() % 4) > 0:
-                mesh_stream.write(b'\x00')
-            primitive["indices"] = len(gltf_data['accessors'])
-            gltf_data['accessors'].append({"bufferView" : buffer_view,\
-                "byteOffset": block_offset,\
-                "componentType": gltf_fmts[subvbs['data'][subindex]['indexBufferIndex']]['componentType'],\
-                "count": len([index for triangle in submesh['ib'] for index in triangle]),\
-                "type": gltf_fmts[subvbs['data'][subindex]['indexBufferIndex']]['accessor_type']})
-            gltf_data['bufferViews'].append({"buffer": 0,\
-                "byteOffset": len(giant_buffer),\
-                "byteLength": mesh_stream.tell()})
-            mesh_stream.seek(0)
-            giant_buffer += mesh_stream.read()
-            mesh_stream.close()
-            del(mesh_stream)
-            if subvbs['data'][subindex]["indexBufferPrimType"] == 3:
-                primitive["mode"] = 4 #TRIANGLES
-            elif subvbs['data'][subindex]["indexBufferPrimType"] == 4:
-                primitive["mode"] = 5 #TRIANGLE_STRIP
-            else:
-                primitive["mode"] = 0 #POINTS
-            gltf_data['meshes'].append({"primitives": [primitive], "name": "Mesh_{0}".format(subindex)})
-            mesh_nodes.append(len(gltf_data['nodes']))
-            gltf_data['nodes'].append({'mesh': buffer_view, 'name': "Mesh_{0}".format(subindex)})
-            buffer_view += 1
+                    "componentType": gltf_fmt['componentType'],\
+                    "count": len([index for triangle in submesh['ib'] for index in triangle]),\
+                    "type": gltf_fmt['accessor_type']})
+                gltf_data['bufferViews'].append({"buffer": 0,\
+                    "byteOffset": len(giant_buffer),\
+                    "byteLength": ib_stream.tell(),\
+                    "target" : 34963})
+                buffer_view += 1
+                ib_stream.seek(0)
+                giant_buffer += ib_stream.read()
+                ib_stream.close()
+                del(ib_stream)
+                if subvbs['data'][subindex]["indexBufferPrimType"] == 3:
+                    primitive["mode"] = 4 #TRIANGLES
+                elif subvbs['data'][subindex]["indexBufferPrimType"] == 4:
+                    primitive["mode"] = 5 #TRIANGLE_STRIP
+                else:
+                    primitive["mode"] = 0 #POINTS
+                mesh_nodes.append(len(gltf_data['nodes']))
+                gltf_data['nodes'].append({'skin': len(gltf_data['skins']), 'mesh': len(gltf_data['meshes']), 'name': "Mesh_{0}".format(subindex)})
+                gltf_data['meshes'].append({"primitives": [primitive], "name": "Mesh_{0}".format(subindex)})
+                skin_bones = list_of_utilized_bones(submesh, model_skel_data)
+                inv_mtx_buffer = bytes()
+                for i in range(len(skin_bones)):
+                    mtx = Quaternion(model_skel_data['boneList'][skin_bones[i]]['abs_q']).transformation_matrix
+                    [mtx[0,3],mtx[1,3],mtx[2,3]] = model_skel_data['boneList'][skin_bones[i]]['abs_p']
+                    inv_bind_mtx = numpy.linalg.inv(mtx)
+                    inv_bind_mtx = numpy.ndarray.transpose(inv_bind_mtx)
+                    inv_mtx_buffer += struct.pack(e+"16f", *[num for row in inv_bind_mtx for num in row])
+                gltf_data['skins'].append({"inverseBindMatrices": len(gltf_data['accessors']), "joints": skin_bones})
+                gltf_data['accessors'].append({"bufferView" : buffer_view,\
+                    "componentType": 5126,\
+                    "count": len(skin_bones),\
+                    "type": "MAT4"})
+                gltf_data['bufferViews'].append({"buffer": 0,\
+                    "byteOffset": len(giant_buffer),\
+                    "byteLength": len(inv_mtx_buffer)})
+                buffer_view += 1
+                giant_buffer += inv_mtx_buffer
+                del(submesh)
     gltf_data['scenes'][0]['nodes'].extend(mesh_nodes)
     gltf_data['buffers'].append({"byteLength": len(giant_buffer), "uri": g1m_name+'.bin'})
     with open(g1m_name+'.bin', 'wb') as f:
