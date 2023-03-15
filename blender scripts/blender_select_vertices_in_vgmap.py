bdd397558bcc8a7f4b21c7df9a2fdb210084fca8
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2023-03-15 12:25:59 -0700

    Changed into Blender add-on

--- blender scripts/blender_select_vertices_in_vgmap.py
+++ blender scripts/blender_select_vertices_in_vgmap.py
@@ -1,8 +1,14 @@
-# Short script to select vertices included in vgmap file. Run inside Blender.
+# Small add-on to select vertices included in vgmap file. Run inside Blender.
 # GitHub eArmada8/gust_stuff
 
+bl_info = {
+    "name": "Select vertices using VGMap",
+    "blender": (2, 80, 0),
+    "category": "Mesh",
+}
+
 import bpy, os, json
-from bpy.props import StringProperty
+from bpy.props import StringProperty, CollectionProperty
 from bpy_extras.io_utils import ImportHelper
 from bpy.types import Operator
 
@@ -10,8 +16,8 @@ class VertexMatch(Operator, ImportHelper):
 
     bl_idname = "vgmapfinder.open_filebrowser"
     bl_label = "Select File"
-    unselect_if_not_in_vgmap = True
-
+    unselect_if_not_in_vgmap: bpy.props.BoolProperty(name="Unselect non-matching", default=False)
+    files: CollectionProperty(name='File paths', type=bpy.types.OperatorFileListElement)
     filter_glob: StringProperty(
         default='*.vgmap',
         options={'HIDDEN'}
@@ -23,9 +29,11 @@ class VertexMatch(Operator, ImportHelper):
             mesh = ob
             mesh.update_from_editmode()
             vg = {i:mesh.vertex_groups[i].name for i in range(len(mesh.vertex_groups))}
-            with open(self.filepath, 'r') as f:
-                vgmapdata = json.loads(f.read())
-                vgmapbones = list(vgmapdata.keys())
+            vgmapbones = []
+            for file in self.files:
+                with open(os.path.join(os.path.dirname(self.filepath),file.name), 'r') as f:
+                    vgmapdata = json.loads(f.read())
+                    vgmapbones.extend(vgmapdata.keys())
             bpy.ops.object.mode_set(mode='EDIT')
             bpy.ops.mesh.select_mode(type='VERT')
             if self.unselect_if_not_in_vgmap == True:
@@ -37,13 +45,28 @@ class VertexMatch(Operator, ImportHelper):
             bpy.ops.object.mode_set(mode='EDIT')
         return {'FINISHED'}
 
+class VertexMatchMenu(bpy.types.Operator):
+    bl_idname = "object.vertex_match"
+    bl_label = "Select vertices using VGMap"
+    bl_options = {'REGISTER', 'UNDO'}
+    
+    def execute (self, context):
+        bpy.ops.vgmapfinder.open_filebrowser('INVOKE_DEFAULT')
+        return {'FINISHED'}
+
+def menu_func(self, context):
+    self.layout.operator(VertexMatchMenu.bl_idname)
+
 def register():
     bpy.utils.register_class(VertexMatch)
+    bpy.utils.register_class(VertexMatchMenu)
+    bpy.types.VIEW3D_MT_edit_mesh.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_class(VertexMatch)
+    bpy.utils.unregister_class(VertexMatchMenu)
+    bpy.types.VIEW3D_MT_edit_mesh.remove(menu_func)
 
 if __name__ == "__main__":
     register()
-
     bpy.ops.vgmapfinder.open_filebrowser('INVOKE_DEFAULT')
